b0f01dcac39b0ecb38da9bc1571dc19741ce45fe
matejko01 <matejko01@hotmail.com>
2020-10-17 09:46:36 +0200

    deleted testcase

--- testcase/locator.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.common.by import By
-
-class MainPageLocators():
-    GO_BUTTON = (By.ID, "submit")
-    ABOUT_BUTTON = (By.ID, "about")
-
-class PyconSearchLocators():
-    CONFERENCES_AND_WORKSHOPS_BUTTON = (By.XPATH, "//*[@id=\"content\"]/div/section/form/ul/li[2]/h3/a")
